9125c7068eb88c1360a3f3a4c114ea91e5a97fc9
Gabriel Lomba <gabriellomba@hotmail.com>
2018-06-26 23:12:53 -0300

    Add bounding box to the first face detected with the clarifai API

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ box, imageUrl }) => {
   return (
     <div className='flex justify-center ma'>
     <div className='absolute mt2'>
-      <img alt='faces' src={imageUrl} width='500px' height='auto'/>
+      <img id='inputImage' alt='faces' src={imageUrl} width='500px' height='auto'/>
+      <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
     </div>
     </div>
   );
