01df5bafd074d9bb34b000585745c1311ec5a633
Gabriel Lomba <gabriellomba@hotmail.com>
2018-06-26 22:43:22 -0300

    Create Rank and FaceRecognition componentes and use Clarifai API for face detection

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
+
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: 'f541ff5770dd47ccb8c47f05c93b8811'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  onSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(
+        function (response) {
+          console.log('RESPONSE: ', response.outputs[0].data.regions[0].region_info.bounding_box);
+          // do something with response
+        },
+        function (err) {
+          console.log('ERROR: ', err);
+          // there was an error
+        }
+      );
+  }
+
   render() {
     return (
       <div className="App">
+        <Particles className='particles' params={particlesOptions} />
         <Navigation />
         <Logo />
-        <ImageLinkForm />
-        {/*
-        <FaceRecognition />*/}
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className='Tilt br2 shadow-2' options={{ max: 55 }} style={{ height: 150, width: 150 }} >
-                <div class='Tilt-inner pa3'>
+                <div className='Tilt-inner pa3'>
                     <img alt='Brain' src={brain}/>
                 </div>
             </Tilt>
